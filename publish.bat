d12622d245f05c87fb73a256acecc5d4fc0a297a
nightblade9 <nightbladecodes@gmail.com>
2019-04-09 13:10:55 -0400

    Publish small (40MB) package, zips too!

--- publish.bat
+++ publish.bat
@@ -2,6 +2,9 @@ cd AbdullahTheWarrior
 
 REM Source: https://stackoverflow.com/questions/44074121/build-net-core-console-application-to-output-an-exe
 dotnet publish -c Release -r win10-x64 -o out_windows
+
+del windows-release.zip
+powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('AliTheAndroid\out_windows', 'windows-release.zip'); }"
 echo DONE. Output is in the directory AbdullahTheWarrior/out_windows
 
 REM for Linux builds!
