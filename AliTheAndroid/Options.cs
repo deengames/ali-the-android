3b4c8bcbfb6d7553fb9bca79f2058a5378aced5d
nightblade9 <nightbladecodes@gmail.com>
2019-05-23 15:43:31 -0400

    Fix typo

--- AliTheAndroid/Options.cs
+++ AliTheAndroid/Options.cs
@@ -5,6 +5,6 @@ namespace DeenGames.AliTheAndroid
         public const int MonsterMultiplier = 1; // 0, 1x, 10x, 20x more monsters?
         public const bool EnableOmniSight = true;
         public const bool ShowFakeWalls = false;
-        public const bool CanUseStairsAnywhere = false;
+        public const bool CanUseStairsFromAnywhere = true;
     }
 }
