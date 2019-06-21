db546c6bfa3b474ccf4820b91b0bec59b53ddcc1
nightblade9 <nightbladecodes@gmail.com>
2019-06-21 15:35:45 -0400

    Time-limit dungeon tests. Test run freezes but at least we can see the problems.

--- AliTheAndroid.Tests/Model/DungeonTests.cs
+++ AliTheAndroid.Tests/Model/DungeonTests.cs
@@ -22,7 +22,9 @@ namespace DeenGames.AliTheAndroid.Tests.Model
         [TestCase(0)]
         public void ConstructorThrowsIfWidthIsNotPositive(int width)
         {
-            Assert.Throws<ArgumentException>(() => new Dungeon(width, 99));
+            RestrictRuntime(() => {
+                Assert.Throws<ArgumentException>(() => new Dungeon(width, 99));
+            });
         }
 
         [TestCase(-10)]
@@ -30,72 +32,81 @@ namespace DeenGames.AliTheAndroid.Tests.Model
         [TestCase(0)]
         public void ConstructorThrowsIfHeightIsNotPositive(int height)
         {
-            Assert.Throws<ArgumentException>(() => new Dungeon(99, height));
+            RestrictRuntime(() => {
+                Assert.Throws<ArgumentException>(() => new Dungeon(99, height));
+            });
         }
 
         [Test]
         public void ConstructorSetsPlayerAndWidthAndHeight()
         {
-            var dungeon = new Dungeon(24, 31, gameSeed: 1234);
-            Assert.That(dungeon.Player, Is.Not.Null);
-            Assert.That(dungeon.Width, Is.EqualTo(24));
-            Assert.That(dungeon.Height, Is.EqualTo(31));
+            RestrictRuntime(() => {
+                var dungeon = new Dungeon(24, 31, gameSeed: 1234);
+                Assert.That(dungeon.Player, Is.Not.Null);
+                Assert.That(dungeon.Width, Is.EqualTo(24));
+                Assert.That(dungeon.Height, Is.EqualTo(31));
+            });
         }
 
         [Test]
         public void GoToNextFloorChangesFloorAndIncrementsFloorNumberAndGeneratesPowerUps()
         {
-            var dungeon = new Dungeon(50, 40, gameSeed: 999);
-            var currentFloor = dungeon.CurrentFloorNum;
+            RestrictRuntime(() => {
+                var dungeon = new Dungeon(50, 40, gameSeed: 999);
+                var currentFloor = dungeon.CurrentFloorNum;
 
-            dungeon.GoToNextFloor();
-            Assert.That(dungeon.CurrentFloorNum, Is.EqualTo(currentFloor + 1));
-            Assert.That(dungeon.CurrentFloor.PowerUps.Count, Is.GreaterThan(0));
+                dungeon.GoToNextFloor();
+                Assert.That(dungeon.CurrentFloorNum, Is.EqualTo(currentFloor + 1));
+                Assert.That(dungeon.CurrentFloor.PowerUps.Count, Is.GreaterThan(0));
 
-            var firstFloor = dungeon.CurrentFloor;
+                var firstFloor = dungeon.CurrentFloor;
 
-            dungeon.GoToNextFloor();
-            Assert.That(dungeon.CurrentFloorNum, Is.EqualTo(currentFloor + 2));
-            var secondFloor = dungeon.CurrentFloor;
+                dungeon.GoToNextFloor();
+                Assert.That(dungeon.CurrentFloorNum, Is.EqualTo(currentFloor + 2));
+                var secondFloor = dungeon.CurrentFloor;
 
-            Assert.That(secondFloor, Is.Not.EqualTo(firstFloor));
+                Assert.That(secondFloor, Is.Not.EqualTo(firstFloor));
+            });
         }
 
         [Test]
         public void GoToPreviousFloorDecrementsFloorNumberAndPositionsPlayerAtStairsDown()
         {
-            var dungeon = new Dungeon(35, 25, gameSeed: 12323);
-            // Start at B1 / floor 0
-            dungeon.GoToNextFloor();
+            RestrictRuntime(() => {
+                var dungeon = new Dungeon(35, 25, gameSeed: 12323);
+                // Start at B1 / floor 0
+                dungeon.GoToNextFloor();
 
-            dungeon.GoToNextFloor();
-            Assert.That(dungeon.CurrentFloorNum, Is.EqualTo(1));
+                dungeon.GoToNextFloor();
+                Assert.That(dungeon.CurrentFloorNum, Is.EqualTo(1));
 
-            dungeon.GoToPreviousFloor();
-            Assert.That(dungeon.CurrentFloorNum, Is.EqualTo(0));
-            Assert.That(dungeon.Player.X, Is.EqualTo(dungeon.CurrentFloor.StairsDownLocation.X));
-            Assert.That(dungeon.Player.Y, Is.EqualTo(dungeon.CurrentFloor.StairsDownLocation.Y));
+                dungeon.GoToPreviousFloor();
+                Assert.That(dungeon.CurrentFloorNum, Is.EqualTo(0));
+                Assert.That(dungeon.Player.X, Is.EqualTo(dungeon.CurrentFloor.StairsDownLocation.X));
+                Assert.That(dungeon.Player.Y, Is.EqualTo(dungeon.CurrentFloor.StairsDownLocation.Y));
+            });
         }
 
         [Test]
         public void GoToNextFloorDoesntRegeneratePowerUps()
         {
-            var dungeon = new Dungeon(35, 25, gameSeed: 12323);
+            RestrictRuntime(() => {
+                var dungeon = new Dungeon(35, 25, gameSeed: 12323);
 
-            // Start at B2 / floor 1
-            dungeon.GoToNextFloor();
-            dungeon.GoToNextFloor();
-            Assert.That(dungeon.CurrentFloorNum, Is.EqualTo(1));
-            Assert.That(dungeon.CurrentFloor.PowerUps.Count > 0);
+                // Start at B2 / floor 1
+                dungeon.GoToNextFloor();
+                dungeon.GoToNextFloor();
+                Assert.That(dungeon.CurrentFloorNum, Is.EqualTo(1));
+                Assert.That(dungeon.CurrentFloor.PowerUps.Count > 0);
 
-            dungeon.CurrentFloor.PowerUps[0].PickUp();
-            Assert.That(dungeon.CurrentFloor.PowerUps.Count == 0);
+                dungeon.CurrentFloor.PowerUps[0].PickUp();
+                Assert.That(dungeon.CurrentFloor.PowerUps.Count == 0);
 
-            dungeon.GoToPreviousFloor();
-            dungeon.GoToNextFloor();
-            Assert.That(dungeon.CurrentFloorNum, Is.EqualTo(1));
-            Assert.That(dungeon.CurrentFloor.PowerUps.Count == 0);            
+                dungeon.GoToPreviousFloor();
+                dungeon.GoToNextFloor();
+                Assert.That(dungeon.CurrentFloorNum, Is.EqualTo(1));
+                Assert.That(dungeon.CurrentFloor.PowerUps.Count == 0);            
+            });
         }
-
     }
 }
