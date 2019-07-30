19d531ca6875a96eb8222ef02e9f551056f496d6
nightblade9 <nightbladecodes@gmail.com>
2019-07-30 16:22:47 -0400

    Key binder shows currently-selected item

--- AliTheAndroid/Consoles/KeyBindingsStrategy.cs
+++ AliTheAndroid/Consoles/KeyBindingsStrategy.cs
@@ -2,14 +2,26 @@ using System;
 using DeenGames.AliTheAndroid.Accessibility;
 using DeenGames.AliTheAndroid.Enums;
 using DeenGames.AliTheAndroid.Infrastructure.Common;
+using Microsoft.Xna.Framework;
 
 namespace DeenGames.AliTheAndroid.Consoles.SubConsoleStrategies
 {
     // Not a real, true, pure strategy; just a draw/update thing.
     public class KeyBindingsStrategy : AbstractConsole
     {
+        private readonly Color SelectedColour = Palette.Blue;
+        private readonly Color SelectedValueColour = Palette.OffWhite;
+        private readonly Color UnselectedColour = Palette.DarkBlueMuted;
+        private readonly Color UnselectedValueColour = Palette.Grey;
+
         private readonly SadConsole.Cell BorderCell = new SadConsole.Cell(Palette.White, Palette.White, ' ');        
         private readonly SadConsole.Cell BackgroundCell = new SadConsole.Cell(Palette.BlackAlmost, Palette.BlackAlmost, ' ');
+        ConfigurableControl selectedItem;
+
+        public KeyBindingsStrategy()
+        {
+            selectedItem = Options.KeyBindings.Keys.GetEnumerator().Current;
+        }
 
         public void Draw(SadConsole.Console console)
         {
@@ -30,8 +42,11 @@ namespace DeenGames.AliTheAndroid.Consoles.SubConsoleStrategies
 
         private void PrintBinding(SadConsole.Console console, ConfigurableControl control, Key boundKey, int y)
         {
-            console.Print(2, y, $"{control.ToString()}: ", Palette.Blue);
-            console.Print(2 + control.ToString().Length + 2, y, boundKey.ToString(), Palette.OffWhite);
+            var nameColour = control == selectedItem ? SelectedColour : UnselectedColour;
+            var keyColour = control == selectedItem ? SelectedValueColour : UnselectedValueColour;
+
+            console.Print(2, y, $"{control.ToString()}: ", nameColour);
+            console.Print(2 + control.ToString().Length + 2, y, boundKey.ToString(), keyColour);
         }
     }
 }
