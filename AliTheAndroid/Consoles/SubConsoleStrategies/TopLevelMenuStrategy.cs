3dfbfe9c24b19ad74464356545419857f1947d7b
nightblade9 <nightbladecodes@gmail.com>
2019-08-14 11:19:01 -0400

    Game-saved indicator in menu

--- AliTheAndroid/Consoles/SubConsoleStrategies/TopLevelMenuStrategy.cs
+++ AliTheAndroid/Consoles/SubConsoleStrategies/TopLevelMenuStrategy.cs
@@ -10,7 +10,9 @@ namespace  DeenGames.AliTheAndroid.Consoles.SubConsoleStrategies
 {
     public class TopLevelMenuStrategy : AbstractConsole, ISubConsoleStrategy
     {
+        private bool justSaved = false; // used to print "saved!" message
         private Player player; 
+
         public TopLevelMenuStrategy(Player player)
         {
             this.player = player;
@@ -21,6 +23,12 @@ namespace  DeenGames.AliTheAndroid.Consoles.SubConsoleStrategies
             console.Print(2, 2, "[D] Review data cubes", Palette.White);
             console.Print(2, 3, "[O] Options", Palette.White);
             console.Print(2, 4, "[S] Save game", Palette.White);
+
+            if (this.justSaved)
+            {
+                console.Print(2, 6, "Game saved!", Palette.Blue);
+            }
+            
             console.Print(2, console.Height - 4, "[ESC] Back to game", Palette.White);
             console.Print(2, console.Height - 3, "[Q] Quit", Palette.White);
         }
@@ -33,20 +41,24 @@ namespace  DeenGames.AliTheAndroid.Consoles.SubConsoleStrategies
                 {
                     InGameSubMenuConsole.IsOpen = false;
                     SadConsole.Global.CurrentScreen = new TitleConsole(Program.GameWidthInTiles, Program.GameHeightInTiles);
+                    this.justSaved = false;
                 }
                 else if (keyboard.IsKeyPressed(Key.O))
                 {
                     EventBus.Instance.Broadcast(GameEvent.ChangeSubMenu, typeof(OptionsMenuStrategy));
+                    this.justSaved = false;
                 }
                 else if (keyboard.IsKeyPressed(Key.D))
                 {
                     EventBus.Instance.Broadcast(GameEvent.ChangeSubMenu, typeof(ShowDataCubesStrategy));
+                    this.justSaved = false;
                 }
                 else if (keyboard.IsKeyPressed(Key.S))
                 {
                     var dungeon = Dungeon.Instance;
                     var serialized = Serializer.Serialize(dungeon);
                     File.WriteAllText(Serializer.SaveGameFileName, serialized);
+                    this.justSaved = true;
                 }
             }
         }
