4bf7f6076dccf294675383ffe9ecbd1dd6b2773f
nightblade9 <nightbladecodes@gmail.com>
2019-09-05 13:01:16 -0400

    Clean up `using` statements

--- AliTheAndroid/Model/Entities/Egg.cs
+++ AliTheAndroid/Model/Entities/Egg.cs
@@ -1,5 +1,4 @@
 using System;
-using DeenGames.AliTheAndroid.Enums;
 using DeenGames.AliTheAndroid.Model.Events;
 using Microsoft.Xna.Framework;
 
@@ -11,6 +10,7 @@ namespace DeenGames.AliTheAndroid.Model.Entities
         private const int hatchProbability = 25; // 20 => 20%
 
         const char DisplayCharacter = (char)233; // Θ
+
         public Egg(int x, int y, Color tenLegsColour) : base("Egg", DisplayCharacter, tenLegsColour, x, y, 1, 0, 0, 0)
         {
             EventBus.Instance.AddListener(GameEvent.PlayerTookTurn, this.TryToHatch);
