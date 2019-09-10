dc11f28befd2d1fa2947930f94c8a54b356a6cee
nightblade9 <nightbladecodes@gmail.com>
2019-09-10 14:52:48 -0400

    Fix publish script - no SFX

--- publish.ps1
+++ publish.ps1
@@ -1,11 +1,15 @@
 $windowsZipFile = 'windows-release.zip'
 
+if (Test-Path("AliTheAndroid\out_windows")) {
+    Remove-Item "AliTheAndroid\out_windows" -Recurse
+}
+
 # Source: https://stackoverflow.com/questions/44074121/build-net-core-console-application-to-output-an-exe
 # Publish to an exe + dependencies. 40MB baseline.
 dotnet publish -c Release -r win10-x64 -o out_windows
 
 # Copy all sound-effects over since we're not using the MonoGame content pipeline
-Copy-Item -Recurse "AliTheAndroid\Content" "AliTheAndroid\out_windows"
+Copy-Item -Recurse "AliTheAndroid\Content" "AliTheAndroid\out_windows\Content"
 
 # Zip it up. ~17MB baseline.
 if (Test-Path($windowsZipFile)) {
