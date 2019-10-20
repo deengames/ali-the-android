918972f7a30f7f51e865753efae45ce582b476da
nightblade9 <nightbladecodes@gmail.com>
2019-10-19 22:53:13 -0400

    Splash screen audio + transition to title

--- AliTheAndroid/Consoles/SplashConsole.cs
+++ AliTheAndroid/Consoles/SplashConsole.cs
@@ -1,4 +1,6 @@
+using System;
 using System.IO;
+using DeenGames.AliTheAndroid.IO;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
@@ -8,6 +10,7 @@ namespace DeenGames.AliTheAndroid.Consoles
     {
         private Texture2D texture;
         private Vector2 position;
+        private DateTime startedOn;
         
         public SplashConsole(int width, int height) : base(width, height)
         {
@@ -19,15 +22,23 @@ namespace DeenGames.AliTheAndroid.Consoles
                 (SadConsole.Global.WindowWidth - texture.Width) / 2,
                 (SadConsole.Global.WindowHeight - texture.Height) / 2);
 
-            // TODO: eventually show title console
-            //Global.CurrentScreen = new TitleConsole(widthInTiles, heightInTiles);
+            startedOn = DateTime.Now;
+            AudioManager.Instance.Play("Nightingale");
+        }
+
+        override public void Update(TimeSpan delta)
+        {
+            if ((DateTime.Now - startedOn).TotalSeconds >= 4.5)
+            {
+                SadConsole.Global.CurrentScreen = new TitleConsole(this.Width, this.Height);
+            }
         }
 
         override public void Draw(System.TimeSpan timeElapsed)
         {
             base.Draw(timeElapsed);
             var drawCall = new SadConsole.DrawCalls.DrawCallTexture(texture, position);
-            SadConsole.Global.DrawCalls.Add(drawCall);
+            SadConsole.Global.DrawCalls.Add(drawCall);            
         }
     }
 }
