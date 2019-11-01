1e5f14558cf7750976e7f5b47484a86a55dc6244
nightblade9 <nightbladecodes@gmail.com>
2019-10-31 20:12:00 -0400

    Publish script properly packages zips `Fonts` directory is needed at runtime Script is also OS agnostic (minus `chmod`) Also packages all platforms required

--- publish.ps1
+++ publish.ps1
@@ -1,25 +1,47 @@
-$windowsZipFile = 'windows-release.zip'
+$ErrorActionPreference = 'Stop'
 
-if (Test-Path("AliTheAndroid\out_windows")) {
-    Remove-Item "AliTheAndroid\out_windows" -Recurse
+$platforms = @('Windows', 'Linux', 'MacOS')
+$rids = @{
+    'Windows' = 'win-x64';
+    'Linux' = 'linux-x64';
+    'MacOS' = 'osx-x64';
 }
+$dirSeparator = [IO.Path]::DirectorySeparatorChar
+$publishDir = "AliTheAndroid" + $dirSeparator + "publish"
 
-# Source: https://stackoverflow.com/questions/44074121/build-net-core-console-application-to-output-an-exe
-# Publish to an exe + dependencies. 40MB baseline.
-dotnet publish -c Release -r win10-x64 -o out_windows
+foreach ($platform in $platforms)
+{
+    $zipFile = "AliTheAndroid-$platform-Beta4.zip"
 
-# Copy all sound-effects over since we're not using the MonoGame content pipeline
-Copy-Item -Recurse "AliTheAndroid\Content" "AliTheAndroid\out_windows\Content"
+    if (Test-Path($publishDir))
+    {
+        Remove-Item $publishDir -Recurse
+    }
 
-# Zip it up. ~17MB baseline.
-if (Test-Path($windowsZipFile)) {
-    Remove-Item $windowsZipFile
+    # Source: https://stackoverflow.com/questions/44074121/build-net-core-console-application-to-output-an-exe
+    # Publish to an exe + dependencies. 40MB baseline.
+    dotnet publish -c Release -r $rids[$platform] -o publish
+
+    $command = ("chmod a+x $publishDir" + $dirSeparator + 'AliTheAndroid')
+    if ($platform -eq 'Windows')
+    {
+        $command += ".exe"
+    }
+    Invoke-Expression $command
+
+    # Copy all content over since we're not using the MonoGame content pipeline
+    foreach ($folder in @('Content', 'Fonts'))
+    {
+        Copy-Item -Recurse ("AliTheAndroid" + $dirSeparator + $folder) ($publishDir + $dirSeparator + $folder)
+    }
+
+    # Zip it up. ~17MB baseline.
+    if (Test-Path($zipFile))
+    {
+        Remove-Item $zipFile
+    }
+
+    Add-Type -A 'System.IO.Compression.FileSystem'
+    [IO.Compression.ZipFile]::CreateFromDirectory($publishDir, $zipFile);
+    Write-Host DONE! Zipped to $zipFile
 }
-
-Add-Type -A 'System.IO.Compression.FileSystem'
-[IO.Compression.ZipFile]::CreateFromDirectory('AliTheAndroid\out_windows', $windowsZipFile);
-Write-Host DONE! Zipped to $windowsZipFile
-
-# for Linux/MacOS builds!
-# dotnet publish -c Release -r ubuntu.16.10-x64 -o out_linux
-# dotnet publish -c Release -r osx.10.11-x64 -o out_macos
