c0dbf8948b747846600f17af545c4ddfba576ac6
nightblade9 <nightbladecodes@gmail.com>
2019-10-31 22:05:58 -0400

    Fix rare race condition that breaks load-game

--- AliTheAndroid/Infrastructure/SaveManager.cs
+++ AliTheAndroid/Infrastructure/SaveManager.cs
@@ -8,6 +8,10 @@ namespace DeenGames.AliTheAndroid.Infrastructure
         public static void SaveGame()
         {
             var dungeon = Dungeon.Instance;
+            
+            // Rare race condition: saving, with effects, causes deserialization error on load
+            dungeon.CurrentFloor.EffectEntities.Clear();
+
             var serialized = Serializer.Serialize(dungeon);
             File.WriteAllText(Serializer.SaveGameFileName, serialized);
         }
