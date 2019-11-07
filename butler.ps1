5a8a33a2f337969232bb2ee3f6f5fcd8d8049da8
nightblade9 <nightbladecodes@gmail.com>
2019-11-06 21:45:59 -0500

    Script to push builds to Butler

---
+++ butler.ps1
@@ -0,0 +1,3 @@
+butler push AliTheAndroid-Linux-v1.0.0.zip deengames/ali-the-android:linux
+butler push AliTheAndroid-MacOS-v1.0.0.zip deengames/ali-the-android:macos
+butler push AliTheAndroid-Windows-v1.0.0.zip deengames/ali-the-android:windows
